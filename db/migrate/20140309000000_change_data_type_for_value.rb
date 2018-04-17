94c4183b7e660757a161e5ea73d58aba7bf4d7e5
Josua Schmid <josua.schmid@renuo.ch>
2018-04-17 14:18:16 +0200

    Version migrations

--- db/migrate/20140309000000_change_data_type_for_value.rb
+++ db/migrate/20140309000000_change_data_type_for_value.rb
@@ -1,4 +1,4 @@
-class ChangeDataTypeForValue < ActiveRecord::Migration
+class ChangeDataTypeForValue < ActiveRecord::Migration[4.2]
   def change
     change_column :spree_sale_prices, :value, :decimal, precision: 10, scale: 2, null: false
   end

0fc532b75078d19731347a62b5869c668c4a88bc
simon <mail@simonisler.ch>
2023-05-23 10:13:41 +0200

    Move decorators

--- lib/solidus_simple_sale_price/engine.rb
+++ lib/solidus_simple_sale_price/engine.rb
@@ -9,15 +9,6 @@ module SolidusSimpleSalePrice
 
     engine_name 'solidus_simple_sale_price'
 
-    def self.activate
-      Dir.glob(File.join(File.dirname(__FILE__), '../../app/**/*_decorator*.rb')).sort.each do |c|
-        Rails.configuration.cache_classes ? require(c) : load(c)
-        Rails.autoloaders.main.ignore(c) if Rails.autoloaders.zeitwerk_enabled?
-      end
-    end
-
-    config.to_prepare(&method(:activate).to_proc)
-
     # use rspec for tests
     config.generators do |g|
       g.test_framework :rspec
